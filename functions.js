7d975a635ee2caa803c5b1c84558512512710f2e
Tom Strat <tomstratfic@hotmail.com>
2019-08-29 20:19:07 +0100

    add more functionality - dragging and resetting

--- functions.js
+++ functions.js
@@ -3,7 +3,17 @@ let mainContainer = document.getElementById("main_container");
 for(let i=0; i<256; i++){
     let div = document.createElement('div');
     div.classList += "gridBlock";
+
+    div.addEventListener("mouseover", function(event){
+        event.target.style.backgroundColor = "black";
+    });
+
     mainContainer.appendChild(div);
+}
+
+function clearDiv(){
+    let grid = document.querySelectorAll("div.gridBlock");
+    grid.forEach()
 }
 
 console.log("done");
